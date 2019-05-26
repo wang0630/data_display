83e59c011711a52bcd9537357c147825c44cb7ad
Chen Yijia <kevinc110604@gmail.com>
2019-05-26 19:37:55 +0800

    Add a function to select time

--- data_display.py
+++ data_display.py
@@ -2,10 +2,16 @@ from display.display import Display
 
 my_display = None
 while(1):
+  print('mode 0: colors depend on time, mode 1: colors depend on positions')
   mode = input('Enter a mode(0~1) or -1 to exit this program: ')
   mode = int(mode)
   
   if mode == 0:
+    time = []
+    start_time = input('Enter the start time, according to the format\nYYYY MM DD\n')
+    time.append(start_time)
+    end_time = input('Enter the end time, according to the format\nYYYY MM DD\n')
+    time.append(end_time)
     pos = input('Enter a position(0~8) or -1 to exit this program: ')
     # Enter a number?
     try:
@@ -14,7 +20,7 @@ while(1):
         pos_list = [pos]
         if my_display:
           my_display.reset()
-        my_display = Display(pos_list)
+        my_display = Display(pos_list, time)
         my_display.get_data()
         my_display.plt_scatter_time()
       elif pos == -1:
@@ -29,6 +35,11 @@ while(1):
 
   elif mode == 1:
     pos_list = []
+    time = []
+    start_time = input('Enter the start time, according to the format\nYYYY MM DD\n')
+    time.append(start_time)
+    end_time = input('Enter the end time, according to the format\nYYYY MM DD\n')
+    time.append(end_time)
     while True:
       pos = input('Enter a position(0~8) one by one until you enter -1 to plot the graph: ')
       pos = int(pos)
@@ -37,7 +48,7 @@ while(1):
       if pos == -1:
         if my_display:
           my_display.reset()
-        my_display = Display(pos_list)
+        my_display = Display(pos_list, time)
         my_display.plt_figure()
         for i in range(len(pos_list)):
           my_display.get_data()
--- display/display.py
+++ display/display.py
@@ -6,8 +6,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 class Display():
-  def __init__(self, pos):
+  def __init__(self, pos, time):
     self.pos = pos
+    taipei_tz = pytz.timezone('Asia/Taipei')
+    self.start_time = dt.datetime.strptime(time[0], '%Y %m %d').replace(tzinfo=taipei_tz)
+    self.end_time = dt.datetime.strptime(time[1], '%Y %m %d').replace(tzinfo=taipei_tz)
     self.index = 0
   def get_data(self):
     r = requests.get(f'http://140.116.82.93:6800/campus/display/{ self.pos[self.index] }')
@@ -50,6 +53,9 @@ class Display():
         color_arr.append(0)
     # Set color_arr to the third column of df for colouring
     df['color'] = color_arr
+    # Select the duration
+    df = df.loc[ df['date'] > self.start_time ]
+    df = df.loc[ df['date'] < self.end_time ]
     plt.figure(figsize=(400, 10))
     labels = ['0~6', '6~12', '12~18', '18~24']
     colors = ['navy', 'turquoise', 'darkorange', 'y']
@@ -74,9 +80,12 @@ class Display():
     # Add explicitly converter
     pd.plotting.register_matplotlib_converters()
     df = pd.DataFrame(self.data)
-
+    # Select the duration
+    df = df.loc[ df['date'] > self.start_time ]
+    df = df.loc[ df['date'] < self.end_time ]
+    # Plot y versus x(time)
     colors = ['navy', 'turquoise', 'darkorange', 'olive', 'lightgray', 'pink', 'lightgreen']
-    l = 'position %d' % self.pos[self.index]
-    
-    plt.plot(df['date'], df['pm25'], c=colors[self.index], label=l, lw=2)
+    label = 'position %d' % self.pos[self.index]
+    plt.plot(df['date'], df['pm25'], c=colors[self.index], label=label, lw=1, ls='-', marker = '.', alpha=0.8)
     self.index = self.index + 1
+  
